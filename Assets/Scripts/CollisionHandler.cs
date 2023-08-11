b9a604e49b99205115e4a060c63786d5ec2bcaa8
Adrien Brake <adnbrake@gmail.com>
2023-08-10 23:42:10 -0230

    Particle Effects, Code Cleanup, and Loss/Success Sounds

--- Assets/Scripts/CollisionHandler.cs
+++ Assets/Scripts/CollisionHandler.cs
@@ -7,9 +7,44 @@ public class CollisionHandler : MonoBehaviour
 {
 
     [SerializeField] float reloadDelay = 1f;
+    [SerializeField] AudioClip crashSound;
+    [SerializeField] AudioClip successSound;
+
+    [SerializeField] ParticleSystem crashParticles;
+    [SerializeField] ParticleSystem successParticles;
+
+    AudioSource audioSource;
+
+    bool isTransitioning = false;
+    bool collisionDisabled = false;
+
+    void Start()
+    {
+        audioSource = GetComponent<AudioSource>();
+    }
+
+    void Update() 
+    {
+        ProcessDebug();
+    }
+
+    void ProcessDebug()
+    {
+        if (Input.GetKeyDown(KeyCode.L))
+        {
+            NextLevel();
+        }
+
+        else if (Input.GetKeyDown(KeyCode.C))
+        {
+            collisionDisabled = !collisionDisabled; //toggle collision
+        }
+    }
 
     void OnCollisionEnter(Collision other) 
     {
+        if (isTransitioning || collisionDisabled) { return; }
+
         switch (other.gameObject.tag)
         {
             case "Friendly":
@@ -23,24 +58,27 @@ public class CollisionHandler : MonoBehaviour
             default:
                 StartCrashSequence();
                 break;
-
         }
     }
     
 
     void StartCrashSequence()
     {
-        //add SFX upon crash
-        //add particle effect upon crash
+        isTransitioning = true;
+        audioSource.Stop();
         GetComponent<Movement>().enabled = false;
+        crashParticles.Play();
+        audioSource.PlayOneShot(crashSound);
         Invoke("ReloadLevel", reloadDelay);
     }
 
     void StartLevelSequence()
     {
-        //add SFX upon success
-        //add particle effect upon success
+        isTransitioning = true;
+        audioSource.Stop();
         GetComponent<Movement>().enabled = false;
+        successParticles.Play();
+        audioSource.PlayOneShot(successSound);
         Invoke("NextLevel", reloadDelay);
     }
 
--- Assets/Scripts/Movement.cs
+++ Assets/Scripts/Movement.cs
@@ -6,57 +6,85 @@ using UnityEngine;
 public class Movement : MonoBehaviour
 {
 
-    Rigidbody rb;
-    AudioSource audioSource;
+    //Tuning Parameters
     [SerializeField] float mainThrust = 1500f;
     [SerializeField] float rotateSpeed = 10f;
+    [SerializeField] AudioClip mainFlight;
 
-    // Start is called before the first frame update
+    [SerializeField] ParticleSystem thrustParticles;
+
+    //CACHE (readability/speed)
+    Rigidbody rb;
+    AudioSource audioSource;
+
+    //STATE
+
     void Start()
     {
         rb = GetComponent<Rigidbody>();
         audioSource = GetComponent<AudioSource>();
     }
 
-    // Update is called once per frame
     void Update()
     {
         ProcessThrust();
         ProcessRotation();
     }
 
+//Processing Methods from here
+
     void ProcessThrust()
     {
         if (Input.GetKey(KeyCode.Space))
         {
-            rb.AddRelativeForce(Vector3.down * mainThrust * Time.deltaTime);
-
-            if (!audioSource.isPlaying)
-            {
-                audioSource.Play();
-            }
+            StartThrusting();
         }
 
         else
         {
-            //StartCoroutine(FadeMixerGroup.StartFade(AudioMixer FlippingPages, String masterVolume, float 3, float 0));
-            //^ is an attempt to fade... did not work.
-            //using https://johnleonardfrench.com/how-to-fade-audio-in-unity-i-tested-every-method-this-ones-the-best/#:~:text=There%27s%20no%20separate%20function%20for,script%20will%20do%20the%20rest.
-            audioSource.Stop();
+            StopThrusting();
         }
     }
-    
-    void ProcessRotation()
+
+        void ProcessRotation()
     {
         if (Input.GetKey(KeyCode.A))
         {
+            //rotate left
             ApplyRotation(-rotateSpeed);
         }
 
         else if (Input.GetKey(KeyCode.D))
         {
+            //rotate right
             ApplyRotation(rotateSpeed);
         }
+    }
+
+//Logic Methods from here:
+
+    void StartThrusting()
+    {
+        rb.AddRelativeForce(Vector3.down * mainThrust * Time.deltaTime);
+
+        if (!audioSource.isPlaying)
+        {
+            audioSource.PlayOneShot(mainFlight);
+        }
+
+        if (!thrustParticles.isPlaying)
+        {
+            thrustParticles.Play();
+        }
+    }
+
+        void StopThrusting()
+    {
+        //StartCoroutine(FadeMixerGroup.StartFade(AudioMixer FlippingPages, String masterVolume, float 3, float 0));
+        //^ is an attempt to fade... did not work.
+        //using https://johnleonardfrench.com/how-to-fade-audio-in-unity-i-tested-every-method-this-ones-the-best/#:~:text=There%27s%20no%20separate%20function%20for,script%20will%20do%20the%20rest.
+        audioSource.Stop();
+        thrustParticles.Stop();
     }
 
     void ApplyRotation(float rotationThisFrame)
